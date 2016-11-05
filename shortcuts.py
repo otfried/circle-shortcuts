8e9d8dd7a3dd263ac64ab0d705f4d3c3b8e4cc7e
Otfried Cheong <otfried@airpost.net>
2016-11-05 22:24:02 +0100

    Updated lemma numbers, small simplification

--- shortcuts.py
+++ shortcuts.py
@@ -12,11 +12,11 @@ from math import *
 lem_antipodal = 11
 lem_upto6combined = 13
 thm_6to7 = 15
-lem_Sp6 = 19
-lem_s2dht = 20
-lem_s2longvi = 21
-lem_thetaset = 22
-lem_bound_combining_edges = 23
+lem_Sp6 = 18
+lem_s2dht = 19
+lem_s2longvi = 20
+lem_thetaset = 21
+lem_bound_combining_edges = 22
 
 # alpha(a)
 def shortcutAngle(a):
@@ -206,7 +206,7 @@ def seven():
   print(  "===================================\n")
   v = 1.7
   print("1.7 + lambda6 = %1.4f > pi" % (v+l6))
-  print("delta(1.999) = %1.4f < 0.54 ==> a^ > 1.999" % delta(1.999))
+  print("delta(1.999) = %1.4f < 0.54" % delta(1.999))
   w1 = pi - 1.999 - 0.54
   w2 = pi - l6 - 0.54
   w3 = pi - v - 0.54
@@ -219,8 +219,7 @@ def seven():
   print("\nSeven shortcuts, Proof of Lemma %d:" % lem_s2longvi)
   print(  "===================================\n")
   dht = ds - 2 * delta(s6)
-  print("delta(1.9997) = %1.5f < %1.5f = d^" % (delta(1.9997), dht))
-  print("4 * d^ * pi = %1.4f" % (4 * dht * pi))
+  print("d^ = d* - 2 * delta(sigma6) = %1.4f" % dht)
   s3 = 0.8 * pi - dht
   print("s3 <= 0.8 * pi - d^ = %1.4f" % s3)
   A1 = 4 * delta(l6) * (pi - l6 - dht)
@@ -230,16 +229,18 @@ def seven():
         % A1)
   print("A(s3, d^) <= 4 * delta(%1.4f) * (pi - %1.4f - d^) = %1.4f" %
         (s3,s3,A2))
-  print("A(a^, d^) < 4 * d^ * (pi - 1.999 - d^) = %1.4f" % A3)
-  print("%1.4f + %1.4f + 4 * %1.4f = %1.4f" % (A1, A2, A3, A1 + A2 + 4 * A3))
+  print("delta(1.999) = %1.5f < d^" % delta(1.999))
+  print("A(1.999, d^) < 4 * d^ * (pi - 1.999 - d^) = %1.4f" % A3)
+  print("%1.4f + %1.4f + 4 * %1.4f = %1.4f < %1.4f = 4 * d^ * pi" %
+        (A1, A2, A3, A1 + A2 + 4 * A3, 4 * dht * pi))
   #
   print("\nSeven shortcuts, Proof of Lemma %d:" % lem_thetaset)
   print(  "===================================\n")
   print("0.4 - d*/2 = %1.4f, 0.4 + d*/2 = %1.4f" %
         (0.4 - ds/2, 0.4 + ds/2))
   # 
-  print("\nSeven shortcuts, a short shortcut")
-  print(  "=================================\n")
+  print("\nSeven shortcuts, a short shortcut exists")
+  print(  "========================================\n")
   w = pi - 1.999 - ds
   print("pi - 1.999 - d* = %1.4f" % w)
   print("pi - 2 * 0.4 = %1.4f > %1.4f = 4 * %1.4f" % (pi - 2 * 0.4, 4 * w, w))
